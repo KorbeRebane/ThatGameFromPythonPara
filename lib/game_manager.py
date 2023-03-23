66fb2463504887af89302f83986bd6cf3bb0c1c7
KorbeRebane <128638879+KorbeRebane@users.noreply.github.com>
2023-03-23 16:37:59 +0300

    Add files via upload

---
+++ lib/game_manager.py
@@ -0,0 +1,38 @@
+from lib.background import Background
+from lib.constants import FIRST_PLATFORM_POSITION, SECOND_PLATFORM_POSITION,FIRST_ENEMY_POSITION, SECOND_ENEMY_POSITION
+from lib.enemy import Enemy
+from lib.interface import GameScreen
+from lib.player import Player
+from lib.platform import Platform
+
+
+class GameManager:
+
+    def __init__(self, sound_manager):
+        self.sound_manager = sound_manager
+        self.player = Player()
+        self.platforms = [Platform(FIRST_PLATFORM_POSITION), Platform(SECOND_PLATFORM_POSITION)]
+        self.background = Background()
+        self.interface = GameScreen()
+        self.enemies = [Enemy(FIRST_ENEMY_POSITION)]
+        self.platforms_rects = []
+
+        sound_manager.play_music("back_music")
+
+    def draw(self, surface):
+        self.background.draw_game_window(surface)
+        self.player.draw(surface)
+        self.interface.draw_health_points(surface, health_points_count=self.player.get_player_health_points())
+        # for enemy in self.enemies:
+            # enemy.draw(surface)
+        for platform in self.platforms:
+            platform.draw(surface)
+
+    def update(self, pressed_keys, upped_keys):
+        for platform in self.platforms:
+            self.platforms_rects.append(platform.rect)
+        self.player.move(pressed_keys, upped_keys, self.platforms_rects)
+        for enemy in self.enemies:
+            enemy.get_damage(self.player.rect)
+        new_state = self.player.game_states()
+        return new_state
